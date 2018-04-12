194244703a0deb5d79ea573978af0e8f26386a7a
Lam Dang <tunglam2051998@gmail.com>
2018-04-12 17:15:31 -0400

    almost final

--- knn.py
+++ knn.py
@@ -47,17 +47,59 @@ def classify(knn):
 		KNN_labels.append(item[0])
 	return most_common(KNN_labels)
 
-def main(filename, k, split, seed):
+
+def get_result_label(dataset):
+	labels = []
+	for row in dataset:
+		if row[0] not in labels:
+			labels.append(row[0])
+	return labels
+
+
+
+def confusion_matrix_util(filename, k, split, seed):
 	trainingSet, testSet = load_data(filename, seed, split)
 	correct = 0
 	total = len(testSet)
+	actual = {}
+	for actual_label in get_result_label(testSet):
+		actual[actual_label] = {}
+		for prediction in get_result_label(testSet):
+			actual[actual_label][prediction] = 0
 	for item in testSet:
 		knn = findKNN(item, trainingSet, k)
 		pred_label = classify(knn)
-		if pred_label == item[0]:
-			correct += 1
-	accuracy = correct/total
-	return accuracy
+		if pred_label not in actual[item[0]]:
+			actual[item[0]][pred_label] = 1
+		else:
+			actual[item[0]][pred_label] += 1
+
+	return actual
+def get_unique(lst):
+	unique = []
+	for item in lst:
+		if item not in unique:
+			unique.append(item)
+	return unique
+
+def main(filename, k, split, seed):
+	con_mat = confusion_matrix_util(filename, k, split, seed)
+	pred_labels = []
+	pred_and_actual = []
+	for key, val in con_mat.items():
+		newEntry = []
+		for pred_label, freq in val.items():
+			pred_labels.append(pred_label)
+			newEntry.append(freq)
+		newEntry.append(key)
+		pred_and_actual.append(newEntry)
+	pred_labels = [get_unique(pred_labels)]
+	conf_mat_list = pred_labels + pred_and_actual
+	display = ""
+	for item in conf_mat_list:
+		display = display + "	".join(str(x) for x in item) + "\n"
+	return display
+
 
 #print(load_data("iris.csv", 12345, 0.75))
-print(main("iris.csv", 3, 0.5, 12345))
+print(main("mnist_large.csv", 3, 0.75, 12345))
